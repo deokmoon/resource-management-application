e4f8b39d6933c32263b30237a8de252f6b8426b5
deokmoon <deok2moon@gmail.com>
2023-11-26 23:36:43 +0900

    [REFACTOR] JPA Repository 오타 수정

--- src/main/java/com/server/resource/management/domain/repository/ServerResourceRepository.java
+++ src/main/java/com/server/resource/management/domain/repository/ServerResourceRepository.java
@@ -1,6 +1,7 @@
 package com.server.resource.management.domain.repository;
 
+import com.server.resource.management.domain.ServerResource;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ServerResourceRepository extends JpaRepository<ServerResourceRepository, Long> {
+public interface ServerResourceRepository extends JpaRepository<ServerResource, Long> {
 }
