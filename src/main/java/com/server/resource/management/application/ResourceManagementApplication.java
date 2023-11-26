2fc6239fc7d2982bc5398f30d33c37a9b86522d4
deokmoon <deok2moon@gmail.com>
2023-11-26 18:25:57 +0900

    init commit

---
+++ src/main/java/com/server/resource/management/application/ResourceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.server.resource.management.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceManagementApplication.class, args);
+    }
+
+}
