c1b4ac2c6cabfa5ffeb82a1e1a02190d66e77f43
deokmoon <deok2moon@gmail.com>
2023-11-28 22:51:20 +0900

    [CHORE] FLY WAY 스키마 정의 및 DEFAULT 값 INSERT

--- src/main/java/com/server/resource/management/domain/repository/UserRepository.java
+++ src/main/java/com/server/resource/management/domain/repository/UserRepository.java
@@ -4,5 +4,9 @@ package com.server.resource.management.domain.repository;
 import com.server.resource.management.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByName(String userName);
 }
---
+++ src/main/resources/db/migration/V2__insert_default_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO server_resource (name, total_cpu, total_memory) VALUES ('서버-1', 20, 100);
+INSERT INTO server_resource (name, total_cpu, total_memory) VALUES ('서버-2', 30, 500);
